ffdbdd01984ada43fdbc416a09a7443aaa2accee
Matthew B White <whitemat@uk.ibm.com>
2020-07-16 14:06:43 +0100

    Update the ReadState and StateExists
    Updates the Read State and State Exists to permit basic operations
    
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- internal/fabric_proxy.go
+++ internal/fabric_proxy.go
@@ -4,8 +4,10 @@
 package internal
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/golang/protobuf/proto"
 	contract "github.com/hyperledgendary/fabric-ledger-protos-go/contract"
 )
 
@@ -14,7 +16,6 @@ func CreateState(request *contract.CreateStateRequest) ([]byte, error) {
 	context := request.Context
 	state := request.State
 	log.Printf("CreateState txid %s chid %s key %s value length %d\n", context.TransactionId, context.ChannelId, state.Key, len(state.Value))
-
 	// stub := &shim.ChaincodeStub{
 	// 	TxID:      context.TransactionId,
 	// 	ChannelID: context.ChannelId,
@@ -40,6 +41,46 @@ func ReadState(request *contract.ReadStateRequest) ([]byte, error) {
 	stash := GetStash()
 	stub := stash.GetStub()
 
+	response := &contract.ReadStateResponse{}
+	state := &contract.State{}
 	log.Printf("ReadState done")
-	return stub.GetState(request.StateKey)
+	stateBytes, err := stub.GetState(request.StateKey)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read from world state. %s", err.Error())
+	}
+
+	state.Key = request.StateKey
+	state.Value = stateBytes
+	response.State = state
+
+	log.Printf("Read State done")
+	return proto.Marshal(response)
+}
+
+// ExistsState is great
+func ExistsState(request *contract.ExistsStateRequest) ([]byte, error) {
+	context := request.Context
+	log.Printf("ExistsState txid %s chid %s key %s\n", context.TransactionId, context.ChannelId, request.StateKey)
+
+	// stub := &shim.ChaincodeStub{
+	// 	TxID:      context.TransactionId,
+	// 	ChannelID: context.ChannelId,
+	// }
+	stash := GetStash()
+	stub := stash.GetStub()
+
+	stateBytes, err := stub.GetState(request.StateKey)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read from world state. %s", err.Error())
+	}
+
+	response := &contract.ExistsStateResponse{}
+
+	if stateBytes == nil {
+		response.Exists = false
+	} else {
+		response.Exists = true
+	}
+
+	return proto.Marshal(response)
 }
