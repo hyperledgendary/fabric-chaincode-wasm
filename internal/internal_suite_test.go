b4d6640ea38699a4d6382e69e759b0eb90104e4d
James Taylor <jamest@uk.ibm.com>
2020-08-21 11:08:14 +0100

    Add get state by range
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- internal/internal_suite_test.go
+++ internal/internal_suite_test.go
@@ -1,7 +1,10 @@
 // Copyright the Hyperledger Fabric contributors. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -o fakes/stub.go --fake-name ChaincodeStubInterface github.com/hyperledger/fabric-chaincode-go/shim.ChaincodeStubInterface
+//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
+
+//counterfeiter:generate -o fakes/stub.go --fake-name ChaincodeStubInterface github.com/hyperledger/fabric-chaincode-go/shim.ChaincodeStubInterface
+//counterfeiter:generate -o fakes/state_query_iterator.go --fake-name StateQueryIteratorInterface github.com/hyperledger/fabric-chaincode-go/shim.StateQueryIteratorInterface
 
 package internal_test
 
